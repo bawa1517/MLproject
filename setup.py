c656a183bbf19f77ef3b76d782e22b2baa18d01a
Nikhilesh Sharma <221030003@juitsolan.in>
2025-03-09 10:51:36 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages,setup
+
+
+
+setup(
+    name = 'MLproject',
+    version = '0.0.1',
+    author = 'Nikhilesh Sharma',
+    author_email = '221030003@juitsolan.in',
+    packages = find_packages(),
+    install_requires = ['pandas','numpy','seaborn','matplotlib']
+    
+    
+)
